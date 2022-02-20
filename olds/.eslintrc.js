8fe9be9ceafab4dc3096715f5bd4bf116b40548e
Spice-Z <15419227+Spice-Z@users.noreply.github.com>
2022-02-20 15:46:35 +0900

    chore: eslint config for old build

---
+++ olds/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  rules: {
+    // turn on errors for missing imports
+    'import/no-unresolved': 'error',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_$' }],
+    'no-nested-ternary': 'off',
+    'no-plusplus': 'off',
+    'no-shadow': 'off',
+    'no-underscore-dangle': 'off',
+    'no-param-reassign': 'off'
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: { project: './' },
+    },
+  },
+};
